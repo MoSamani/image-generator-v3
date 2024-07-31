99509fdfec2d3098908a931e80748d62e1e8e180
Mohammad Samani <mosam102@uni-duesseldorf.de>
2024-07-31 14:38:19 +0200

    Added radio- und textinputs

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -10,24 +10,123 @@ const Homepage = () => {
         <p className=".roboto-bold">Think outside the box!</p>
       </header>
 
-      <main>
+      <main className="roboto-regular">
         <div className="main">
           <div className="herobox1">
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
-              vero facere voluptatum ut sint error id nam eaque itaque quam
-              neque optio, quisquam, beatae qui hic quos architecto deserunt
-              sunt?
-            </p>
+            <form method="post">
+              <div className="inputWrapper form">
+                <p className="inputLabel">Eras</p>
+                <div className="radioGruop">
+                  <label>
+                    <input
+                      type="radio"
+                      name="Eras"
+                      id="Stone age"
+                      value="Stone age"
+                    ></input>
+                    Stone age
+                  </label>
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="Eras"
+                      id="Middle age"
+                      value="Middle age"
+                    ></input>
+                    Middle age
+                  </label>
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="Eras"
+                      id="Baroque"
+                      value="Baroque"
+                    ></input>
+                    Baroque
+                  </label>
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="Eras"
+                      id="Future"
+                      value="Future"
+                    ></input>
+                    Future
+                  </label>
+                </div>
+
+                <p className="inputLabel">Location</p>
+                <div className="radioGruop">
+                  <label>
+                    <input
+                      type="radio"
+                      name="Location"
+                      id="Asia"
+                      value="Asia"
+                    ></input>
+                    Asia
+                  </label>
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="Location"
+                      id="America"
+                      value="America"
+                    ></input>
+                    America
+                  </label>
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="Location"
+                      id="Africa"
+                      value="Africa"
+                    ></input>
+                    Africa
+                  </label>
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="Location"
+                      id="Europe"
+                      value="Europe"
+                    ></input>
+                    Europe
+                  </label>
+                </div>
+
+                <p className="inputLabel"> What kind of clothes do you like?</p>
+                <input
+                  type="text"
+                  name="Clothes"
+                  id="Clothes"
+                  placeholder="Leather shoes, Kimono, Saris or ..."
+                ></input>
+
+                <p className="inputLabel"> How old are you?</p>
+                <input
+                  type="text"
+                  name="Old"
+                  id="Old"
+                  placeholder="20, 58, ..."
+                ></input>
+
+                <p className="inputLabel"> Let us know about yourself</p>
+                <textarea
+                  name="Promt"
+                  id="Promt"
+                  placeholder="Hobbys, ..."
+                ></textarea>
+              </div>
+            </form>
           </div>
-          <div className="herobox2">
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
-              vero facere voluptatum ut sint error id nam eaque itaque quam
-              neque optio, quisquam, beatae qui hic quos architecto deserunt
-              sunt?
-            </p>
-          </div>
+          <div className="herobox2"></div>
         </div>
         <button className="nextbutton">Next</button>
       </main>
